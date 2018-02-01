8c8b6fb7c0945703b42e17c3ae2c138556b63217
Slobodan Kovrlija <slobkov02@yahoo.com>
2018-02-01 01:58:01 -0500

    Add UI elements for task 1

---
+++ OTG/OTG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTG
+//
+//  Created by Slobodan Kovrlija on 2/1/18.
+//  Copyright © 2018 Slobodan Kovrlija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
