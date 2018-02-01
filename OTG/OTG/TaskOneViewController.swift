e9c5acad158daa371c079efc6e3821381640e2f8
Slobodan Kovrlija <slobkov02@yahoo.com>
2018-02-01 02:53:41 -0500

    Add done button to number pad

--- OTG/OTG/TaskOneViewController.swift
+++ OTG/OTG/TaskOneViewController.swift
@@ -14,21 +14,52 @@ class TaskOneViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var integerTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        integerTextField.delegate = self
+        addDoneButtonOnKeyboard()
     }
 
+    // MARK: - Text Field Delegate
     func textFieldDidEndEditing(_ textField: UITextField) {
         
         if let numberString = integerTextField.text {
             let enteredNumber = Int(numberString)
             if let enteredNumber = enteredNumber {
                 let numberOfOnes = countOnesIn(n: enteredNumber)
-                resultLabel.text = "There are: \(numberOfOnes) 1s"
+                resultLabel.text = "There are: \(numberOfOnes)  1s."
             }
         }
+    }
+    
+    // Adds done button on number pad keyboard
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        doneToolbar.barStyle = UIBarStyle.default
         
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                        target: nil,
+                                        action: nil)
+    
+        let doneButton: UIBarButtonItem = UIBarButtonItem(title: "Done",
+                                                          style: .done,
+                                                          target: self,
+                                                          action: #selector(doneButtonAction))
+        
+        let items = NSMutableArray()
+        items.add(flexSpace)
+        items.add(doneButton)
+        
+        doneToolbar.items = items as? [UIBarButtonItem]
+        doneToolbar.sizeToFit()
+        
+        integerTextField.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        integerTextField.resignFirstResponder()
     }
 
     // Returns the number of 1-s in binary representation of n
