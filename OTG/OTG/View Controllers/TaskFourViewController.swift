b48454037e87a240da7a4386e4574933e26df8d1
Slobodan Kovrlija <slobkov02@yahoo.com>
2018-02-01 21:00:15 -0500

    Add task 4 view controller

---
+++ OTG/OTG/View Controllers/TaskFourViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TaskFourViewController.swift
+//  OTG
+//
+//  Created by Slobodan Kovrlija on 2/1/18.
+//  Copyright © 2018 Slobodan Kovrlija. All rights reserved.
+//
+
+import UIKit
+
+class TaskFourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
